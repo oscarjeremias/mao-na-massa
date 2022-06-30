ec715e0d06ad3e79966ab110e243e1873b4e91de
Oscar jeremias jc <96687872+oscarjeremias@users.noreply.github.com>
2022-06-30 11:00:17 +0100

    Update index.tsx component perfilDev

--- src/components/perfilDev/index.tsx
+++ src/components/perfilDev/index.tsx
@@ -7,7 +7,7 @@ export function PerfilDev() {
 			<div>
 				<img src="https://github.com/oscarjeremias.png" alt="foto do drv que criou o blog" width="150px" className="rounded-full border border-blue-400" />
 			</div>
-			<div className="my-4 py-3 border border-white border-t-0 border-r-0 border-l-0 border-b text-2xl">Oscar Jeremias JC</div>
+			<div className="my-4 py-3 border border-white border-t-0 border-r-0 border-l-0 border-b text-2xl">Oscar Jeremias jc</div>
 			<div className="my-4 py-3 border birder-white border-t-0 border-r-0 border-l-0 border-b">
 				Eu sou o Óscar jeremias um desenvolvedor front-end, que gosta de compartilhar conhecimento com a comunidade dev e tem com objetivo princípal da carreira melhor a experência do usuário na web
 			</div>
